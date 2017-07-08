64d8e1e97474e979df9927d8d8dad40133c809d8
Eren <julian07027@gmail.com>
2017-07-09 00:43:38 +0800

    update ETL_104

--- ETL_104E.py
+++ ETL_104E.py
@@ -1,21 +1,27 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-res = requests.get("https://www.104.com.tw/job/?jobno=4twky&jobsource=104_hotorder")
-soup = bs(res.text,"lxml")
-keyword = ['公司福利','工程師']
-level =['專科','大學''碩士']
-lang = ['linux','java','javascript','mysql',"ms sql",'不拘','android','jsp', 'ios app']+level+keyword
+res = requests.get('https://www.104.com.tw/job/?jobno=4twky&jobsource=104_hotorder')
+soup = bs(res.text, 'lxml')
+
+# keyword = ['公司福利', '工程師']
+# level =['專科', '大學', '碩士']
+# lang = ['不拘', 'linux', 'java', 'javascript', 'mysql', 'ms sql', 'android', 'jsp',\
+#         'ios app', 'c', 'c++', 'python', 'ruby'] + level + keyword
+
+lang = ['不拘', 'linux', 'java', 'javascript', 'mysql', 'ms sql', 'android', 'jsp',\
+        'ios app', 'c', 'c++', 'python', 'ruby', 'scala', 'hadoop', 'spark']
+
 tmp = soup.findAll('title')
 print(tmp)
 lib = []
 for tmp in soup.find_all('section'):
     tmp2 = "".join(tmp.text.lower().strip())
-    for xx in lang:
-        if tmp2.find(xx) >= 0:
+    for ti in lang:
+        if tmp2.find(ti) >= 0:
             if tmp2 not in lib:
                 # lib.append(tmp2)
-                print('>> ' + xx.title() + ' <<')
+                print(ti.title())
                 # print(''.join(tmp2.split(':')))
 
 # for tmp in soup.find_all('dd'):
--- ETL_104H.py
+++ ETL_104H.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup as bs
 from selenium.webdriver.support.select import Select
 import requests
 import time
-browser = webdriver.Chrome()
-# browser = webdriver.Firefox()
-
+browser = webdriver.Chrome('E://PycharmProjects/selenium_driver/chromedriver')
 browser.maximize_window()
-url='https://www.104.com.tw/jobbank/joblist/joblist.cfm?indcat=1001001002&area=6001001000&jobsource=104_bank1'
-p=1
+
+url = 'https://www.104.com.tw/jobbank/joblist/joblist.cfm?\
+       indcat=1001001002&area=6001001000&jobsource=104_bank1'
+page = 1
 while True:
     browser.get(url)
-    soup = bs(browser.page_source,'lxml')
+    soup = bs(browser.page_source, 'lxml')
     # tmp1 = soup.select('li > div > a')[4:] #li > div > a
     # print(browser.find_element('li > div > a'))
-    tmp2 = soup.findAll(attrs={"class" : "jobname_summary job_name"})
+    tmp2 = soup.findAll(attrs={"class": "jobname_summary job_name"})
     tmp3 = tmp2
-    c=1
-    for xx in tmp3:
+    count = 1
+    for ti in tmp3:
         # print(c,xx.text.strip())
-        print(c,xx.text.strip(),"\t\t\t\t\t\t\t\t\t\t\t",str(xx).split("a href=")[1].split()[0])
-        c+=1
-    p=p+1
-    url='https://www.104.com.tw/jobbank/joblist/joblist.cfm?jobsource=104_bank1&ro=0&area=6001001000&indcat=1001001002&order=2&asc=0&page='+str(p)+'&psl=N_A'
+        print(count, ti.text.strip(), "\t", str(ti).split("a href=")[1].split()[0])
+        count += 1
+    page = page + 1
+    url = 'https://www.104.com.tw/jobbank/joblist/joblist.cfm?jobsource=104_bank1&ro=0\
+           &area=6001001000&indcat=1001001002&order=2&asc=0&page=' + str(page) + '&psl=N_A'
     time.sleep(5)
-
